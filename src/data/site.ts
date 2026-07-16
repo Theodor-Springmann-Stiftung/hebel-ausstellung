2ad5d29230bdd7593a23d4c00bce4600e96c495a
Simon Martens <simon.martens@mailbox.org>
2026-07-16 16:30:24 +0200

    +Tablet regression

---
+++ src/data/site.ts
@@ -0,0 +1,9 @@
+export const site = {
+  title: "Das Nachleben der Worte",
+  tagline: "Johann Peter Hebel",
+  footerTagline: "Eine virtuelle Ausstellung der Theodor Springmann Stiftung",
+  repositoryUrl: "https://github.com/Theodor-Springmann-Stiftung/hebel-ausstellung/",
+  copyright: "© 2026 Theodor Springmann Stiftung",
+};
+
+export const pageTitle = `${site.tagline}: ${site.title}. Eine Ausstellung.`;
