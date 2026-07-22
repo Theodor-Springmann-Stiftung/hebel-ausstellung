193e50932d49af62dc629ba06fdfe49c8181822e
Simon Martens <simon.martens@mailbox.org>
2026-07-22 17:38:30 +0200

    +GH Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dev.ausstellung.hebel-archiv.de',
   devToolbar: {
     enabled: false
   },
