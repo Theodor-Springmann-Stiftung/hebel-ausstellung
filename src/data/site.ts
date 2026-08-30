81814b1909ec579c1033882d609f2d9354866990
Simon Martens <simon.martens@mailbox.org>
2026-08-30 17:01:32 +0200

    +Meta-Tags

--- src/data/site.ts
+++ src/data/site.ts
@@ -6,4 +6,8 @@ export const site = {
   copyright: "© 2026 Theodor Springmann Stiftung",
 };
 
-export const pageTitle = `${site.tagline}: ${site.title}. Eine Ausstellung.`;
+export const productionUrl = "https://ausstellung.hebel-archiv.de";
+export const seoSiteName = "Das Nachleben der Worte. Johann Peter Hebels Werk und Wirken.";
+export const pageTitle = "Johann Peter Hebel – Das Nachleben der Worte";
+export const exhibitionCredit = "Virtuelle Ausstellung: Johann Peter Hebel. Das Nachleben der Worte. Erarbeitet und kuratiert von Alexander Knopf, Jakob Brüssermann und Andreas Beck.";
+export const homeDescription = "Eine virtuelle Ausstellung zu Leben und Werk des Schriftstellers Johann Peter Hebel. Erarbeitet und kuratiert vom Hebel-Archiv der Theodor Springmann Stiftung in Heidelberg.";
