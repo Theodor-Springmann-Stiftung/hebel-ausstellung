c68d9afad28429cd6d294858a0f499987ea3d6b4
Simon Martens <simon.martens@mailbox.org>
2026-08-30 17:20:16 +0200

    +Landmarks, itemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dev.ausstellung.hebel-archiv.de',
+  site: 'https://ausstellung.hebel-archiv.de',
   devToolbar: {
     enabled: false
   },
+  integrations: [
+    sitemap({
+      filter(page) {
+        const pathname = new URL(page).pathname;
+
+        return !['/404', '/404/', '/404.html'].includes(pathname) && !pathname.endsWith('.json');
+      }
+    })
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
